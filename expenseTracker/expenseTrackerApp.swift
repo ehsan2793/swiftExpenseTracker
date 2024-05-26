4455b745a8bc4e3eba7c53b85e75e8268d5bb34a
ehsan2793 <ehsan2793@gmail.com>
2024-05-26 11:33:28 -0700

    Initial Commit

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Ehsan Rahimi on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
